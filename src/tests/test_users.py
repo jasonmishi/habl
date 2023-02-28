9b661a9626ae78643efb6198d119ebb02e8c4886
Jason Mishike <jasonmishi@gmail.com>
2023-02-28 22:43:38 +0530

    add testing through pytest

---
+++ src/tests/test_users.py
@@ -0,0 +1,9 @@
+import pytest
+
+from django.contrib.auth.models import User
+
+
+@pytest.mark.django_db
+def test_user_create():
+  User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+  assert User.objects.count() == 1
