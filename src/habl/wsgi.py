63fe806e15d5c9787933d2fec924526ed59f9575
Jason Mishike <jasonmishi@gmail.com>
2023-02-28 21:56:38 +0530

    add linting using pre-commit

--- src/habl/wsgi.py
+++ src/habl/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'habl.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "habl.settings")
 
 application = get_wsgi_application()
