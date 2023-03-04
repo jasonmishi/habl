2e33003901f35cce9db2417ea713a9c53836d37d
Jason Mishike <jasonmishi@gmail.com>
2023-03-04 23:19:58 +0530

    add Profile model

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -1,9 +1,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+
+from .models import User
 
 
 class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100, help_text="Last Name")
+    last_name = forms.CharField(max_length=100, help_text="Last Name")
     username = forms.CharField(max_length=30)
     email = forms.EmailField(max_length=200)
 
@@ -11,6 +14,8 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = (
             "username",
+            "first_name",
+            "last_name",
             "email",
             "password1",
             "password2",
