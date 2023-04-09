14d84ee933b31e8b4230f20f202629952f529e97
Jason Mishike <jasonmishi@gmail.com>
2023-04-09 18:44:26 +0530

    fix first_name help_text in SignUpForm

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -5,7 +5,7 @@ from .models import User
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100, help_text="Last Name")
+    first_name = forms.CharField(max_length=100, help_text="First Name")
     last_name = forms.CharField(max_length=100, help_text="Last Name")
     username = forms.CharField(max_length=30)
     email = forms.EmailField(max_length=200)
